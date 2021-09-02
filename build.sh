086278d741aa2ad57692ac3b81389cd763bdaa0b
南浦月 <nanpuyue@gmail.com>
2021-09-03 01:40:14 +0800

    chore: use vendored-sources for docker builder

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 
 docker build builder -t avatar-bot-builder
+[ -d .cargo ] || mkdir .cargo
+cargo vendor > .cargo/config.toml
 docker run -it --rm -v "$PWD":/build/avatar-bot --workdir /build/avatar-bot avatar-bot-builder\
  cargo build --release --target x86_64-unknown-linux-musl
