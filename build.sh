fc590b89a5e44285b662f4c94519411962449743
南浦月 <nanpuyue@gmail.com>
2022-03-30 21:15:02 +0800

    chore: mount cargo registry cache

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 docker build builder -t avatar-bot-builder
-[ -d .cargo ] || mkdir .cargo
-cargo vendor > .cargo/config.toml
-docker run -it --rm -v "$PWD":/build/avatar-bot --workdir /build/avatar-bot avatar-bot-builder\
+
+[ -d "$CARGO_HOME" ] || CARGO_HOME="$HOME/.cargo"
+WORKDIR="/build/avatar-bot"
+REGISTRY="/usr/local/cargo/registry"
+
+docker run -it --rm -v "$PWD":"$WORKDIR" -v "$CARGO_HOME/registry":"$REGISTRY"\
+ --workdir "$WORKDIR" avatar-bot-builder\
  cargo build --release --target x86_64-unknown-linux-musl
