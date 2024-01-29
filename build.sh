f91de3963a6b940c956ba2f7a937d3153faf8bdd
南浦月 <nanpuyue@gmail.com>
2024-01-29 17:03:53 +0800

    chore: update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,26 @@
 #!/bin/bash -ex
 
-RUSTARCH="$(uname -m)"
-[ "$RUSTARCH" = arm64 ] && RUSTARCH=aarch64
+if [ "$1" = x86_64 ] || [ "$1" = aarch64 ]; then
+    RUSTARCH="$1"
+else
+    RUSTARCH="$(uname -m)"
+    [ "$RUSTARCH" = arm64 ] && RUSTARCH=aarch64
+fi
+
+if [ "$RUSTARCH" = x86_64 ];then
+    PLATFORM=linux/amd64
+elif [ "$RUSTARCH" = aarch64 ]; then
+    PLATFORM=linux/arm64
+else
+    echo "Unsupported platform!"
+    exit 1
+fi
 
 [ -d "$CARGO_HOME" ] || CARGO_HOME="$HOME/.cargo"
 WORKDIR="/build/avatar-bot"
 REGISTRY="/usr/local/cargo/registry"
 
-docker run -it --rm -v "$CARGO_HOME/registry":"$REGISTRY"\
+docker run --platform="$PLATFORM" -it --rm -v "$CARGO_HOME/registry":"$REGISTRY"\
  -v "$PWD":"$WORKDIR" --workdir "$WORKDIR"\
  ghcr.io/nanpuyue/avatar-bot-builder:latest\
  cargo build --release --target "$RUSTARCH-unknown-linux-musl"
